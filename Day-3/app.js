025472573541c6110a16f45e3424cff4249b82c1
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-06-24 19:57:59 +0530

    node

---
+++ Day-3/app.js
@@ -0,0 +1,12 @@
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+// 1. Define an Event Listener 
+emitter.on("GREET" , ()=>{
+    console.log("Hello World")
+})
+
+// 2. Trigger ( emit)
+
+emitter.emit("GREET")
